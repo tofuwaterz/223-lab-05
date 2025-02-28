5afa4ccad05f4da29d44a8031494bd0ec308bc11
Luis Salgado <143764445+tofuwaterz@users.noreply.github.com>
2025-02-27 21:41:25 -0800

    changes to main and shopping

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+# Name: Luis Salgado
+# Date: 02/27/25
+# File Purpose: Lab 05 - Main
+"""
+import shopping
+
+def main():
+    # code for main function 
+if __name__ == "__main__":
+    main()
--- shopping.py
+++ shopping.py
@@ -22,8 +22,21 @@ def add_customer_cart(name, **items):
     '''input the customer name  items which holds keys and price value pairs, returns nothing, only print a message that cart was added'''
     print(f"{name}'s cart has been added to the shopping list")
     
+# TOFIX: write the proper implementation for this function
 # lookup_cart(name)
-def lookup_cart(name):
-    '''input the customer name, returns a dictionary of the items in the cart'''
-    print(cart details here)
+#def lookup_cart(name):
+#    '''input the customer name, returns a dictionary of the items in the cart'''
+#    print(cart details here)
 
+# remove_item(name, item)
+def remove_item(name, item):
+    '''input the customer name and the item to be removed, returns nothing, only print a message that item was removed'''
+    print(f"{item} has been removed from {name}'s cart")
+
+# apply_discount(name)
+
+# add_item(name, item, price=0)
+
+# add_item(name, item, price=0)
+
+# sale(queue)
